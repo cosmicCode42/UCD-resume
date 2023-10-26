8efca0aa010efa349e3ca8475810e3c7025830a8
cosmicCode42 <cosmicanswer@gmail.com>
2023-10-26 03:13:44 +0000

    github stuff is workinggit add .git add .

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,7 +2,7 @@ function userInformationHTML(user) {
     return `
         <h2>${user.name}
           <span class="small-name">
-            <a href="${user.html_url}" target="_blank">${user.login}</a>
+            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
           </span>
         </h2>
         <div class="gh-content">
@@ -11,10 +11,12 @@ function userInformationHTML(user) {
               <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
             </a>
           </div>
+          <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`
 }
 
 function fetchGitHubInformation(event) {
+
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -23,11 +25,11 @@ function fetchGitHubInformation(event) {
 
     $("#gh-user-data").html(
         `<div id="loader">
-          <img src="assets/css/loader.gif" alt="loading" />
+            <img src="assets/css/loader.gif" alt="loading" />
         </div>`);
     
     $.when(
-       $.getJSON(`https:/api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`)
     ).then (
         function(response) {
             var userData = response;
